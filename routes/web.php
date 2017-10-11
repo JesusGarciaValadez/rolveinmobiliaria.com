7047707ca3ae796d624210cee477174d0a1766b5
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2017-10-11 18:39:42 -0500

    Update composer

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 /*
 |--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Auth::routes();
+
+/*
+|--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
@@ -14,8 +27,6 @@
 Route::get('/', function () {
   return view('welcome');
 });
-
-Auth::routes();
 
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 
