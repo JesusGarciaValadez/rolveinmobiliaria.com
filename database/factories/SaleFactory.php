2f675bea06aa75c6ec424ab00054b2059dfea803
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2017-10-22 17:39:27 -0500

    Complete migration for Sales

--- database/factories/SaleFactory.php
+++ database/factories/SaleFactory.php
@@ -7,5 +7,47 @@ $factory->define(App\Sale::class, function (Faker $faker) {
     'internal_expedients_id' => function () {
       return factory(App\InternalExpedient::class)->create()->id;
     },
+    $table->integer('sale_documents_id')
+          ->unsigned();
+    $table->foreign('sale_documents_id')
+          ->references('id')
+          ->on('sale_documents')
+          ->onUpdate('cascade')
+          ->onDelete('cascade');
+    $table->integer('sale_closing_contracts_id')
+          ->unsigned();
+    $table->foreign('sale_closing_contracts_id')
+          ->references('id')
+          ->on('sale_closing_contracts')
+          ->onUpdate('cascade')
+          ->onDelete('cascade');
+    $table->integer('sale_logs_id')
+          ->unsigned();
+    $table->foreign('sale_logs_id')
+          ->references('id')
+          ->on('sale_logs')
+          ->onUpdate('cascade')
+          ->onDelete('cascade');
+    $table->integer('sale_contracts_id')
+          ->unsigned();
+    $table->foreign('sale_contracts_id')
+          ->references('id')
+          ->on('sale_contracts')
+          ->onUpdate('cascade')
+          ->onDelete('cascade');
+    $table->integer('sale_notaries_id')
+          ->unsigned();
+    $table->foreign('sale_notaries_id')
+          ->references('id')
+          ->on('sale_notaries')
+          ->onUpdate('cascade')
+          ->onDelete('cascade');
+    $table->integer('sale_signatures_id')
+          ->unsigned();
+    $table->foreign('sale_signatures_id')
+          ->references('id')
+          ->on('sale_signatures')
+          ->onUpdate('cascade')
+          ->onDelete('cascade');
   ];
 });
