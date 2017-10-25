34532595b769ba73caaee16a935db74d1a6f4019
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2017-10-25 01:05:38 -0500

    Fix probabilities for complete registries in sales table

--- database/factories/SaleFactory.php
+++ database/factories/SaleFactory.php
@@ -8,35 +8,47 @@ $factory->define(App\Sale::class, function (Faker $faker) {
     null,
   ]);
 
-  $sale_documents_id = $faker->randomElement([
-    factory(App\SaleDocument::class)->create()->id,
-    null,
-  ]);
+  $sale_documents_id = (empty($internal_expedients_id))
+    ? null
+    : $faker->randomElement([
+        factory(App\SaleDocument::class)->create()->id,
+        null,
+      ]);
 
-  $sale_closing_contracts_id = $faker->randomElement([
-    factory(App\SaleClosingContract::class)->create()->id,
-    null,
-  ]);
+  $sale_closing_contracts_id = (empty($sale_documents_id))
+    ? null
+    : $faker->randomElement([
+        factory(App\SaleClosingContract::class)->create()->id,
+        null,
+      ]);
 
-  $sale_logs_id = $faker->randomElement([
-    factory(App\SaleLog::class)->create()->id,
-    null,
-  ]);
+  $sale_logs_id = (empty($sale_closing_contracts_id))
+    ? null
+    : $faker->randomElement([
+        factory(App\SaleLog::class)->create()->id,
+        null,
+      ]);
 
-  $sale_contracts_id = $faker->randomElement([
-    factory(App\SaleContract::class)->create()->id,
-    null,
-  ]);
+  $sale_contracts_id = (empty($sale_logs_id))
+    ? null
+    : $faker->randomElement([
+        factory(App\SaleContract::class)->create()->id,
+        null,
+      ]);
 
-  $sale_notaries_id = $faker->randomElement([
-    factory(App\SaleNotary::class)->create()->id,
-    null,
-  ]);
+  $sale_notaries_id = (empty($sale_contracts_id))
+    ? null
+    : $faker->randomElement([
+        factory(App\SaleNotary::class)->create()->id,
+        null,
+      ]);
 
-  $sale_signatures_id = $faker->randomElement([
-    factory(App\SaleSignature::class)->create()->id,
-    null,
-  ]);
+  $sale_signatures_id = (empty($sale_notaries_id))
+    ? null
+    : $faker->randomElement([
+        factory(App\SaleSignature::class)->create()->id,
+        null,
+      ]);
 
   return [
     'internal_expedients_id' => $internal_expedients_id,
