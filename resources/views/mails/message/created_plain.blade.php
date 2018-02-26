08400757574f29f46d11c331a07e68710e3432e6
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2018-02-25 22:52:58 -0600

    Fix a link in email of message reated

--- resources/views/mails/message/created_plain.blade.php
+++ resources/views/mails/message/created_plain.blade.php
@@ -1,6 +1,6 @@
 Rolve
 
-{{ $messageCreated->user->name }} atendió a [{{ $messageCreated->name }}] ({{ route('show_message', ['id', $messageCreated->id ]) }}) con las siguientes observaciones:
+{{ $messageCreated->user->name }} atendió a [{{ $messageCreated->name }}] ({{ route('show_message', ['id' => $messageCreated->id ]) }}) con las siguientes observaciones:
 
 {{ $messageCreated->observations }}
 
