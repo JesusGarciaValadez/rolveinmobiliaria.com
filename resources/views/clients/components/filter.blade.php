19053e72554c4d51af8a2b503d0d60ee6ff03337
Jesús García <zerovamp@hotmail.com>
2018-07-02 18:05:36 -0500

    Adding edit a filter component to clients sections.

--- resources/views/clients/components/filter.blade.php
+++ resources/views/clients/components/filter.blade.php
@@ -1,6 +1,6 @@
 <div class="panel panel-default">
   <form
-    id="call-filter"
+    id="client__filter"
     class="panel-heading form-horizontal clearfix"
     role="search"
     action="{{ route('filter_client') }}"
