a7e765e45cc8c147a8a341aa86799f99f520fa78
Jesús García <zerovamp@hotmail.com>
2018-07-05 22:44:34 -0500

    Edit method of SaleClosingContract passing parameters

--- app/Http/Controllers/SaleClosingContractController.php
+++ app/Http/Controllers/SaleClosingContractController.php
@@ -3,41 +3,144 @@
 namespace App\Http\Controllers;
 
 use App\SaleClosingContract;
+use App\Sale;
+use App\Client;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Carbon\Carbon as Carbon;
+use Illuminate\Support\Facades\DB as DB;
+use Illuminate\Support\Facades\Auth as Auth;
 
 class SaleClosingContractController extends Controller
 {
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\SaleClosingContract  $saleClosingContract
-     * @return \Illuminate\Http\Response
-     */
-    public function show(SaleClosingContract $saleClosingContract)
-    {
-      return view('sales.edit_closing_contract');
-    }
+  use ThrottlesLogins;
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\SaleClosingContract  $saleClosingContract
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, SaleClosingContract $saleClosingContract)
-    {
-        //
-    }
+  /**
+   * Set the uri returned to views.
+   *
+   * @var string
+   */
+  private $_uri = 'for_sales';
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\SaleClosingContract  $saleClosingContract
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(SaleClosingContract $saleClosingContract)
-    {
-        //
-    }
+  /**
+   * Set the localization for the language in the app.
+   *
+   * @var string
+   */
+  private $_locale;
+
+  /**
+   * Set the message to the used returned to views.
+   *
+   * @var string
+   */
+  private $_message = null;
+
+  /**
+   * Set the type of the alarm return to views.
+   *
+   * @var string
+   */
+  private $_type = null;
+
+  /**
+   * Date of the attribute updated or created.
+   *
+   * @var string
+   */
+  private $_date = null;
+
+  public function __constructor ()
+  {
+    $this->_locale = \App::getLocale();
+  }
+
+  /**
+   * Display a listing of the resource.
+   *
+   * @param  \App\Sale  $sale
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Sale $sale)
+  {
+      //
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @param  \App\Sale  $sale
+   * @return \Illuminate\Http\Response
+   */
+  public function create(Sale $sale)
+  {
+      //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Sale  $sale
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request, Sale $sale)
+  {
+      //
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Sale  $sale
+   * @param  \App\SaleClosingContract  $saleClosingContract
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Sale $sale, SaleClosingContract $saleClosingContract)
+  {
+      //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Sale  $sale
+   * @param  \App\SaleClosingContract  $saleClosingContract
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id, $closingContractId, Sale $sale, SaleClosingContract $saleClosingContract)
+  {
+    $sale = Sale::findOrFail($id);
+    $clients = Client::all();
+
+    return view('sales.edit_closing_contract')
+            ->withUri($this->_uri)
+            ->withSale($sale)
+            ->withClients($clients);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Sale  $sale
+   * @param  \App\SaleClosingContract  $saleClosingContract
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Sale $sale, SaleClosingContract $saleClosingContract)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Sale  $sale
+   * @param  \App\SaleClosingContract  $saleClosingContract
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Sale $sale, SaleClosingContract $saleClosingContract)
+  {
+      //
+  }
 }
