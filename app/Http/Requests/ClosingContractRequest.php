d8dc3ca713efe181e2cf8d85a0756ced01c787f5
Jesús García <zerovamp@hotmail.com>
2018-07-13 00:10:00 -0500

    Rename the Sale requests without the Sale prefix.

--- app/Http/Requests/ClosingContractRequest.php
+++ app/Http/Requests/ClosingContractRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class SaleClosingContractRequest extends FormRequest
+class ClosingContractRequest extends FormRequest
 {
   /**
    * Determine if the user is authorized to make this request.
