cd1d837d4c62d629c457d5cc1b86b16d3c15d6aa
Jesús García <zerovamp@hotmail.com>
2018-09-10 20:35:51 -0500

    Adding contract and notary requests

---
+++ app/Http/Requests/ContractRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ContractRequest extends FormRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return \Auth::check();
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'SC_infonavit_contracts_id' => 'nullable|string',
+      'SC_fovissste_contracts_id' => 'nullable|string',
+      'SC_cofinavit_contracts_id' => 'nullable|string',
+      'SC_mortgage_broker' => 'nullable|string',
+      'SC_contract_with_the_broker' => 'nullable|string',
+      'SC_mortgage_credit' => [
+        'string',
+        Rule::in([
+          'INFONAVIT',
+          'FOVISSSTE',
+          'COFINAVIT',
+          'Bancario',
+          'Aliados',
+        ]),
+      ],
+      'SC_general_buyer' => 'nullable|string',
+      'SC_purchase_agreements' => 'nullable|string',
+      'SC_tax_assessment' => 'nullable|string',
+      'SC_notary_checklist' => 'nullable|string',
+      'SC_notary_file' => 'nullable|string',
+      'SC_complete' => 'nullable|string'
+    ];
+  }
+}
---
+++ app/Http/Requests/NotaryRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class NotaryRequest extends FormRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return \Auth::check();
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'SN_federal_entity' => 'nullable|string',
+      'SN_notaries_office' => 'nullable|string',
+      'SN_string_freedom_of_lien_certificate' => 'nullable|string',
+      'SN_hour_freedom_of_lien_certificate' => 'nullable|string',
+      'SN_observations_freedom_of_lien_certificate' => 'nullable|string',
+      'SN_beginning_of_the_certificate_of_freedom_of_assessment' => 'nullable|string',
+      'SN_zoning' => 'nullable|string',
+      'SN_water_no_due_constants' => 'nullable|string',
+      'SN_non_debit_proof_of_property' => 'nullable|string',
+      'SN_certificate_of_improvement' => 'nullable|string',
+      'SN_key_and_cadastral_value' => 'nullable|string',
+      'SN_seller_documents' => 'nullable|string',
+      'SN_buyer_documents' => 'nullable|string',
+      'SN_activation_documents_for_the_mortgage_loan' => 'nullable|string',
+    ];
+  }
+}
