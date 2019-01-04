6ca934451948deba276e008ce45fe017ed784c55
Jesús Antonio García Valadez <jesus.garciav@me.com>
2019-01-04 13:44:03 -0600

    Linted code and added id.
    Signed-off-by: Jesús Antonio García Valadez <jesus.garciav@me.com>

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -3,28 +3,30 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta http-equiv="Cache-control" content="private">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  @env('local')
 
-  <link rel="canonical" href="{{ url()->current() }}" />
+  @else
+    <meta http-equiv="Cache-control" content="private">
+    @endenv
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title')</title>
+    <title>@yield('title')</title>
 
-  <link rel="canonical" href="{{ url()->current() }}">
-  <!-- Styles -->
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="canonical" href="{{ url()->current() }}">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-  <style media="screen">
-    [v-cloak] {
-      display: none;
-    }
-  </style>
+    <style media="screen">
+      [v-cloak] {
+        display: none;
+      }
+    </style>
 </head>
 <body>
-  <div>
+  <div id="app">
     @navbar()
     @endnavbar
 
@@ -32,14 +34,11 @@
   </div>
 
   <!-- Scripts -->
+  <script src="{{ asset('js/lang.js') }}"></script>
+  <script src="{{ asset('js/enums.js') }}"></script>
   <script src="{{ asset('js/manifest.js') }}"></script>
   <script src="{{ asset('js/vendor.js') }}"></script>
   <script src="{{ mix('js/app.js') }}"></script>
-  <script>
-    $(function () {
-      $('[data-toggle="tooltip"]').tooltip()
-    })
-  </script>
   @yield('scripts')
 </body>
 </html>
